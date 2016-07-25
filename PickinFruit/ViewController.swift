ad3451e8304ba11d6d59e0e87a146eb42c44fe06
hdinhof1 <hdinhof1@binghamton.edu>
2016-07-25 13:17:06 -0400

    All tests pass

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,24 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
+        self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        
+        self.fruitPicker.delegate = self
+        self.fruitPicker.dataSource = self
         
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
+        let firstRandomItemIndex = Int(arc4random_uniform(UInt32(self.fruitsArray.count)))
+        let secondRandomItemIndex = Int(arc4random_uniform(UInt32(self.fruitsArray.count)))
+        let thirdRandomItemIndex = Int(arc4random_uniform(UInt32(self.fruitsArray.count)))
         
         
+        self.fruitPicker.selectRow(firstRandomItemIndex, inComponent: 0, animated: true)
+        self.fruitPicker.selectRow(secondRandomItemIndex, inComponent: 1, animated: true)
+        self.fruitPicker.selectRow(thirdRandomItemIndex, inComponent: 2, animated: true)
+        
+        let firstItem = self.fruitsArray[self.fruitPicker.selectedRowInComponent(0)]
+        let secondItem = self.fruitsArray[self.fruitPicker.selectedRowInComponent(1)]
+        let thirdItem = self.fruitsArray[self.fruitPicker.selectedRowInComponent(2)]
+        
+        print("\(firstRandomItemIndex) \(secondRandomItemIndex) \(thirdRandomItemIndex)")
+        print(firstItem + secondItem + thirdItem)
+        
+        if firstItem == secondItem && secondItem == thirdItem {
+            self.resultLabel?.text = "WINNER!"
+        } else {
+            self.resultLabel?.text = "TRY AGAIN"
+        }
+    }
+    //DataSource protocol methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3 //3 columns
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.fruitsArray.count
+    }
+    
+    //Delegate protocol methods
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.fruitsArray[row]
     }
     
 }
